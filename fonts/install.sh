949870af1b68feaeea10666f869c5aefc2f25a9d
Matthew Moulton <matthew.moulton@gmail.com>
2016-09-05 19:15:11 -0400

    add font installation

---
+++ fonts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo ""
+echo "[\033[0;32mINFO\033[0m] install fonts"
+
+# Fonts from:
+# https://github.com/google/fonts
+# https://github.com/powerline/fonts
+cp fonts/*/*.ttf /Library/Fonts/
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sh homebrew/install.sh
-sh git/install.sh
+
 sh bash/install.sh
+sh fonts/install.sh
+sh git/install.sh
