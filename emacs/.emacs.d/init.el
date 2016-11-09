a97ce150b3c40eb8ebf291e2ca92cbdd0ac0f7f9
Matthew Moulton <matthew.moulton@gmail.com>
2016-11-09 17:19:49 -0600

    remove custom generated code

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -147,17 +147,3 @@
 (define-key org-mode-map (kbd "M-j") 'org-metadown)
 (define-key org-mode-map (kbd "M-k") 'org-metaup)
 (define-key org-mode-map (kbd "M-l") 'org-metaright)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-	(yasnippet web-mode s powerline neotree markdown-mode less-css-mode helm-projectile helm evil-surround evil color-theme-solarized coffee-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
