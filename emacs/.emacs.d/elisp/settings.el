f442381cb78998bd8b7a57d692af3af0c806605a
Matthew Moulton <matthew.moulton@gmail.com>
2017-02-24 07:57:35 -0500

    add org mode files

--- emacs/.emacs.d/elisp/settings.el
+++ emacs/.emacs.d/elisp/settings.el
@@ -17,6 +17,7 @@
 (setq neo-smart-open nil)
 (setq neo-theme 'arrow)
 (setq neo-window-width 40)
+(setq org-agenda-files '("~/SpiderOak Backup/Org"))
 (setq org-log-done t)
 (setq org-startup-folded 'showall)
 (setq projectile-completion-system 'helm)
