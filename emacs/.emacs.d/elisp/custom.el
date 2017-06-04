b07c6aad1edbbd9968c5aa604aee639feacb3a87
Matthew Moulton <matthew.moulton@gmail.com>
2017-06-04 11:49:50 -0500

    configure org-mode

--- emacs/.emacs.d/elisp/custom.el
+++ emacs/.emacs.d/elisp/custom.el
@@ -3,9 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
+ '(org-agenda-files (quote ("~/Notes")))
+ '(org-directory "~/Notes")
+ '(org-link-frame-setup
    (quote
-	(which-key neotree helm-projectile helm evil))))
+    ((vm . vm-visit-folder-other-frame)
+     (vm-imap . vm-visit-imap-folder-other-frame)
+     (gnus . org-gnus-no-new-news)
+     (file . find-file-other-window)
+     (wl . wl-other-frame))))
+ '(org-log-done (quote time))
+ '(org-startup-folded (quote showall))
+ '(package-selected-packages (quote (which-key neotree helm-projectile helm evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
