89b3373847b868fd8bed059b164f718cd9658f00
Matthew Moulton <matthew.moulton@gmail.com>
2017-10-21 14:58:33 -0400

    add ivy/swiper faces

--- emacs/.emacs.d/elisp/custom.el
+++ emacs/.emacs.d/elisp/custom.el
@@ -6,7 +6,7 @@
  '(custom-enabled-themes (quote (solarized)))
  '(custom-safe-themes
    (quote
-    ("3812c2be930e4a496be3dbab258bdcfcbfb52c2bd13df02a52e4c2fdfd920205" "02f80c931a5b2d37b02ac7f19f2faea74f0c4d363530a5f27575548631d8874b" "2311c7904233c155d155b6182e2bbac1113fc1adce051bac35192fe44d3b6d80" "f4edd5c0132d03d9d1614c394da29a954842cf5fe8843e4f1c5acb27d2eccc23" default)))
+    ("2ed15926a7502f1f88b50602e9ed154de3f007d6e95530189d2651979f376f96" default)))
  '(custom-theme-directory "~/.emacs.d/themes/")
  '(package-selected-packages (quote (projectile which-key use-package neotree evil))))
 (custom-set-faces
