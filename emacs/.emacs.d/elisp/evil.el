50109bd61a77385e6f63348f0eaf7786dc8108cd
Matthew Moulton <matthew.moulton@gmail.com>
2017-10-28 09:35:34 -0400

    start magit in motion state

--- emacs/.emacs.d/elisp/evil.el
+++ emacs/.emacs.d/elisp/evil.el
@@ -3,9 +3,16 @@
   :demand
   :diminish 'undo-tree-mode
   :bind (:map evil-insert-state-map
-              ("TAB" . self-insert-command))
-  :init 
+              ("TAB" . self-insert-command)
+              :map evil-motion-state-map
+              ("RET" . nil)
+              :map evil-normal-state-map
+              ("RET" . evil-ret))
+  :init
   (setq evil-want-C-i-jump nil) ; Enable TAB for org mode
   (setq evil-want-C-u-scroll t)
-  :config 
-  (evil-mode))
+  :config
+  (evil-mode)
+  (evil-set-initial-state 'magit-diff-mode 'motion)
+  (evil-set-initial-state 'magit-log-mode 'motion)
+  (evil-set-initial-state 'magit-status-mode 'motion))
