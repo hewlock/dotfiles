1c43c0df969739fbbb523983e563b52f6572de1b
Matthew Moulton <matthew.moulton@gmail.com>
2017-10-31 07:39:29 -0400

    more eww key binds

--- emacs/.emacs.d/elisp/eww.el
+++ emacs/.emacs.d/elisp/eww.el
@@ -1,7 +1,8 @@
 (use-package eww
   :ensure t
   :bind (("C-c e s" . eww)
-         ("C-c e b" . eww-list-bookmarks))
+         ("C-c e b" . eww-list-bookmarks)
+         ("C-c e S" . eww-reload))
   :init
   (evil-set-initial-state 'eww-bookmark-mode 'motion)
   (evil-set-initial-state 'eww-mode 'motion))
