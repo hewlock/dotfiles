918b77bc42ceb1c8f6c126ff44c6509d071a7353
Matthew Moulton <matthew.moulton@gmail.com>
2017-10-31 07:32:33 -0400

    keybinds for eww

--- emacs/.emacs.d/elisp/eww.el
+++ emacs/.emacs.d/elisp/eww.el
@@ -1,5 +1,7 @@
 (use-package eww
   :ensure t
+  :bind (("C-c e s" . eww)
+         ("C-c e b" . eww-list-bookmarks))
   :init
   (evil-set-initial-state 'eww-bookmark-mode 'motion)
   (evil-set-initial-state 'eww-mode 'motion))
--- emacs/.emacs.d/elisp/magit.el
+++ emacs/.emacs.d/elisp/magit.el
@@ -1,6 +1,9 @@
 (use-package magit
   :ensure t
   :diminish 'magit-mode
+  :bind (("C-c g b" . magit-blame)
+         ("C-c g g" . magit-status)
+         ("C-c g l" . magit-log-all))
   :init
   (setq magit-bury-buffer-function (lambda(&optional kill-buffer) (interactive) (magit-restore-window-configuration t)))
   (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
@@ -11,7 +14,4 @@
   (advice-add 'magit-blame-mode :before 'evil-emacs-state)
   (advice-add 'magit-blame-quit :after 'evil-normal-state)
   (diminish 'auto-revert-mode)
-  (diminish 'global-auto-revert-mode)
-  :bind (("C-c g b" . magit-blame)
-         ("C-c g g" . magit-status)
-         ("C-c g l" . magit-log-all)))
+  (diminish 'global-auto-revert-mode))
