176f596d92cc8cb657d2438c5ab76de11edac919
Matthew Moulton <matthew.moulton@gmail.com>
2018-02-17 19:32:49 -0500

    fix font size

--- gnome-terminal/solarized-base.sh
+++ gnome-terminal/solarized-base.sh
@@ -7,7 +7,7 @@ gsettings set org.gnome.Terminal.Legacy.Settings theme-variant "system"
 
 gsettings set "${SOLARIZED_SCHEMA}" bold-color-same-as-fg "true"
 gsettings set "${SOLARIZED_SCHEMA}" cursor-colors-set "true"
-gsettings set "${SOLARIZED_SCHEMA}" font "Roboto Mono for Powerline 12"
+gsettings set "${SOLARIZED_SCHEMA}" font "Roboto Mono for Powerline 11"
 gsettings set "${SOLARIZED_SCHEMA}" highlight-colors-set "true"
 gsettings set "${SOLARIZED_SCHEMA}" scrollbar-policy "never"
 gsettings set "${SOLARIZED_SCHEMA}" use-theme-colors "false"
