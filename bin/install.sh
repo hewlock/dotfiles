bd57ecc97852344b68b7a0c7b9ba9c35e7ec8062
Matthew Moulton <matthew.moulton@gmail.com>
2021-09-22 07:38:06 -0400

    use libsecret for git credential helper

--- bin/install.sh
+++ bin/install.sh
@@ -7,15 +7,23 @@ backup_gsettings
 install curl
 install emacs
 install fortune
+install gcc
 install gnome-disk-utility
 install gnome-system-monitor
 install keepassxc
+install libsecret-1-0
+install libsecret-1-dev
+install make
 install stow
 install tmux
 install tree
 install vim
 
 install "ubuntu-restricted-extras ttf-mscorefonts-installer-"
+
+# Git credential helper
+# https://stackoverflow.com/a/40312117
+cmd "sudo make --directory=/usr/share/doc/git/contrib/credential/libsecret"
 
 link bash
 link emacs
