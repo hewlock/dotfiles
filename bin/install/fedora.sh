a0b2331b75056bc5b7fd75b111ac8de5d8349541
Matthew Moulton <matthew.moulton@gmail.com>
2021-09-27 10:55:46 -0400

    install roboto fonts

--- bin/install/fedora.sh
+++ bin/install/fedora.sh
@@ -2,3 +2,8 @@
 
 install git-credential-libsecret
 cmd "git config --global credential.helper \"libsecret\""
+
+install google-roboto-condensed-fonts
+install google-roboto-fonts
+install google-roboto-mono-fonts
+install google-roboto-slab-fonts
