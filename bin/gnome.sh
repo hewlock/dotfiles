578eb7f1fd571c205ebda46d8e8c824d3636587d
Matthew Moulton <matthew.moulton@gmail.com>
2021-10-02 12:32:03 -0400

    #6 use Emacs gtk-key-theme

--- bin/gnome.sh
+++ bin/gnome.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# For supported keys see: /usr/share/themes/Emacs/gtk-3.0/gtk-keys.css
+cmd "gsettings set org.gnome.desktop.interface gtk-key-theme \"Emacs\""
+
 cmd "gsettings set org.gnome.desktop.input-sources xkb-options \"['grp:alt_shift_toggle', 'ctrl:nocaps', 'compose:menu']\""
 cmd "gsettings set org.gnome.settings-daemon.plugins.media-keys next \"['AudioNext']\""
 cmd "gsettings set org.gnome.settings-daemon.plugins.media-keys play \"['AudioPlay']\""
