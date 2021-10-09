fcd41d3ec938e510c19ad5d48e93376f2fa195b6
Matthew Moulton <matthew.moulton@gmail.com>
2021-10-09 07:20:35 -0400

    add rpmfusion to Fedora install

--- home/.emacs.d/elisp/custom.el
+++ home/.emacs.d/elisp/custom.el
@@ -3,15 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (solarized)))
+ '(custom-enabled-themes '(solarized))
  '(custom-safe-themes
-   (quote
-	("19098e7cee80d3012e6574cff5c9dc7489f868955ba57140eb1fc93acdf2a684" default)))
+   '("19098e7cee80d3012e6574cff5c9dc7489f868955ba57140eb1fc93acdf2a684" default))
  '(custom-theme-directory "~/.emacs.d/themes/")
- '(frame-background-mode (quote dark))
+ '(frame-background-mode 'light)
  '(package-selected-packages
-   (quote
-	(web-mode markdown-mode less-css-mode coffee-mode yasnippet which-key rainbow-delimiters neotree magit counsel swiper ivy-hydra ivy evil-surround company evil use-package))))
+   '(web-mode markdown-mode less-css-mode coffee-mode yasnippet which-key rainbow-delimiters neotree magit counsel swiper ivy-hydra ivy evil-surround company evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
