ad03fbe6531d5135566728c299589373e2464d0a
Matthew Moulton <matthew.moulton@gmail.com>
2021-10-14 06:11:11 -0400

    add org agenda location and keybinds

--- home/.emacs.d/elisp/org.el
+++ home/.emacs.d/elisp/org.el
@@ -4,18 +4,21 @@
          :map org-mode-map
          ("C-c c a" . org-table-align)
          ("C-c c e" . org-html-export-as-html)
-         ("C-c c h" . org-insert-heading-respect-content)
-         ("C-c c t" . org-insert-todo-heading)
          ("C-c c u" . org-update-all-dblocks)
          ("M-H" . org-shiftmetaleft)
          ("M-J" . org-shiftmetadown)
          ("M-K" . org-shiftmetaup)
          ("M-L" . org-shiftmetaright)
          ("M-h" . org-metaleft)
+         ("M-i" . org-insert-todo-heading)
          ("M-j" . org-metadown)
          ("M-k" . org-metaup)
-         ("M-l" . org-metaright))
+         ("M-l" . org-metaright)
+         ("M-o" . org-insert-heading-respect-content)
+		 ("M-t" . mrm/org-toggle-flagged))
   :init
+  (setq org-agenda-files (split-string (shell-command-to-string "find $X_TODOS -type d")))
+  (setq org-directory (getenv "X_TODOS"))
   (setq org-export-with-toc nil)
   (setq org-html-head-include-default-style nil)
   (setq org-html-head-include-scripts nil)
@@ -30,6 +33,14 @@
         (concat "<style>\n"
                 (mrm/get-file-as-string "~/.emacs.d/css/solarized.css")
                 "</style>")))
+
+(defun mrm/org-toggle-flagged ()
+  "Toggle flagged tag to org item"
+  (interactive)
+  (let ((tags (org-get-tags)))
+	(if (member "flagged" tags)
+		(org-set-tags (sort (remove "flagged" tags) 'string<))
+	  (org-set-tags (sort (append '("flagged") tags) 'string<)))))
 
 (defun mrm/org-clocktable-indent-string (level)
   "Fix \emsp from showing in clock tables"
