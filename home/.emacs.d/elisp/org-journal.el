deb934780b8690d4ea15988af60cbc908c37396d
Matthew Moulton <matthew.moulton@gmail.com>
2021-11-12 08:42:58 -0500

    fix journal entries caching date

--- home/.emacs.d/elisp/org-journal.el
+++ home/.emacs.d/elisp/org-journal.el
@@ -1,19 +1,25 @@
 (use-package org-journal
   :ensure t
   :bind (("C-c o c" . calendar)
-         ("C-c o j" . org-journal-new-entry))
+         ("C-c o j" . mrm/org-journal-new-entry))
   :init
   (setq org-journal-date-format "%Y-%m-%d")
-  (setq org-journal-date-prefix (concat
-                                 "#+Title: "
-                                 (format-time-string "%Y-%m-%d")
-                                 "\n#+Subtitle: "
-                                 (format-time-string "%A")
-                                 "\n#+Date: "))
   (setq org-journal-dir (concat "~/Dropbox/Org/Journal/" (format-time-string "%Y") "/"))
   (setq org-journal-file-format "%Y-%m-%d.org")
   (setq org-journal-hide-entries-p nil)
   (setq org-journal-time-format "%-I:%M %p\n\n")
   (setq org-journal-time-prefix "\n* ")
   (evil-set-initial-state 'calendar-mode 'emacs))
+
+(defun mrm/org-journal-new-entry ()
+  "Create a new journal entry.
+Needed to fix cached time in org-journal-date-prefix."
+  (interactive)
+  (setq-default org-journal-date-prefix (concat
+                                         "#+Title: "
+                                         (format-time-string "%Y-%m-%d")
+                                         "\n#+Subtitle: "
+                                         (format-time-string "%A")
+                                         "\n#+Date: "))
+  (org-journal-new-entry nil))
 
