19fdc97ed3410c6f5ece2bec6dc63479fd86f529
Matthew Moulton <matthew.moulton@gmail.com>
2021-12-19 06:46:55 -0500

    cleanup whitespace on buffer save

--- home/.emacs.d/elisp/whitespace.el
+++ home/.emacs.d/elisp/whitespace.el
@@ -1,5 +1,8 @@
 (use-package whitespace
-  :ensure t 
+  :ensure t
   :bind (("C-c i w" . whitespace-mode)
-         ("C-c r w" . whitespace-cleanup))
+         ("C-c b w" . whitespace-cleanup)
+         ("C-c r w" . whitespace-cleanup-region))
   :diminish 'whitespace-mode)
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
